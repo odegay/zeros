f464d72d0e3c4c7ba1076a46a31362e1b9b07adc
Oleg Degay <Oleg_Degay@epam.com>
2018-09-20 22:40:49 +0300

    The solution is available on Inernet: https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%202/123.htm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  //sorry but this is available on Internet
+  //https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%202/123.htm
+  qty = 0;
+  while (number > 0){
+    number = Math.floor(number/5);
+    qty += number;
+  }
+  return qty;
 }
+
+//console.log(getZerosCount(25));
